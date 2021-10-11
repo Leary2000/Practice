18ee0d8754448ce1e7a95c105d631941b21c8279
Leary2000 <92332234+Leary2000@users.noreply.github.com>
2021-10-11 20:08:17 +0100

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -5,7 +5,7 @@ public class Palindrome {
         String input = new String("");
         System.out.println("Please enter a String");
         input = scan.nextLine();
-        if (isPalindrome(input)) //pass the string to the method
+        if (isPalindrome(input))
         { System.out.println("YES!" + input + " IS A PALINDROME");
         } else
         {
@@ -15,11 +15,10 @@ public class Palindrome {
     public static boolean isPalindrome(String s)
     {
         // Base Case for shortest strings 
-
-        if (s.length() <= 1) // AVA
+        if (s.length() <= 1) 
         // must be a palindrome 
         {
-            //System.out.println("Current values of String in base case is"+s);
+         
             return true;
         }
         else
@@ -35,15 +34,14 @@ public class Palindrome {
                 System.out.println("Current values of String in base case where string is not a palindrone"+s);
                 return false;
             }
-            else // Recursive Case
-            { /* * If the first and last chars are equal * then reduce the problem
-                    and call the method * again with the simpler value */
+            else
+            { 
                 System.out.println("Current value of String in recursive case is: "+s);
                 String t=s.substring(1, s.length() - 1);
 
                 System.out.println("New string to be sent to recursive call is: "+t);
                 return (isPalindrome(t));
-                //return (isPalindrome(s.substring(1, s.length() - 1)));
+               
 
             }
         }
